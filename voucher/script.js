3d330ee5a11df1b4cb20d22b45b262529efc2791
bhagirathiaccount <bhagirathiacc00@gmail.com>
2024-10-08 20:08:11 +0530

    Update script.js

--- voucher/script.js
+++ voucher/script.js
@@ -2,7 +2,7 @@ function validateForm() {
     const voucherNo = document.getElementById('voucherNo').value;
     const date = document.getElementById('date').value;
     const amount = document.getElementById('amount').value;
-    const amount = document.getElementById('paid_to').value;
+    const paid_to = document.getElementById('paid_to').value;
     const description = document.getElementById('description').value;
 
     if (!voucherNo || !date || !amount || !paid_to || !description) {
